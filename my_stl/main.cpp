a8f27be0402763b35cc495df8ab68e3d78ebc320
kent <scse11061083@163.com>
2019-11-12 22:11:09 +0800

    添加未提交的文件

--- my_stl/main.cpp
+++ my_stl/main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include "my_stl_vector.h"
+#include "my_stl_tree.h"
 
 int main()
 {
-    my_vector<int> kkk;
-  
-    for (int i = 0; i < 100; ++i)
-    {
-        kkk.push_back(i);
-        cout << kkk.back() << " ";
-        cout << kkk.size() << " " << kkk.capacity() << endl;
-    }
+    rb_tree<int, int, my_identity<int>, less<int>> itree;
+    cout << itree.size() << endl;
+    itree.insert_unique(10);
+    itree.insert_unique(7);
+    itree.insert_unique(8);
+    itree.insert_unique(15);
+    itree.insert_unique(5);
+    itree.insert_unique(6);
+    itree.insert_unique(11);
+    itree.insert_unique(13);
+    itree.insert_unique(12);
 
-    for (int i = 0; i < 100; ++i)
+    cout << itree.size() << endl;
+    auto it = itree.begin();
+    __rb_tree_base_iterator rbit;
+    for (; it != itree.end(); ++it)
     {
-        cout << kkk[i] << " ";
+        rbit = __rb_tree_base_iterator(it);
+        cout << *it << "(" << rbit.node->color << ")" << " ";
     }
     cout << endl;
-
     int aa;
     cin >> aa;
     return 0;
